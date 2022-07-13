9a8504e7cb08540c36cb34ad73289039e0a479d5
KosmasKaisaris <73787322+KosmasKaisaris@users.noreply.github.com>
2022-07-13 10:59:39 +0300

    Add files via upload

--- UdemyTutorial/udemyTutorialChapter8/Customer.java
+++ UdemyTutorial/udemyTutorialChapter8/Customer.java
@@ -1,10 +1,32 @@
-package udemyTutorialChapter8;
+package udemyTutorialXrwstoumenaCh8;
 
 import java.util.ArrayList;
 
 public class Customer {
 	private String name;
+	private ArrayList<Double> transactions;
 	
-	ArrayList<Double> arrayDouble = new ArrayList<Double>();
+	public Customer(String name, double initialAmount) {
+		this.name = name;
+		this.transactions= new ArrayList<Double>();
+		addTransaction(initialAmount);
+		
+	}
+	
+	public void addTransaction(double amount) {
+		this.transactions.add(amount);
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public ArrayList<Double> getTransactions(){
+		return transactions;
+	}
+	
+	
+	
 
 }
