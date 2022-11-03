800defad88d7d24148cfddc067f3128a332c975a
KosmasKaisaris <73787322+KosmasKaisaris@users.noreply.github.com>
2022-11-03 12:11:22 +0200

    Add files via upload

---
+++ UdemyTutorial/udemyTutorialChapter18/MainBanking.java
@@ -0,0 +1,7 @@
+package udemyTutorialChapter18;
+
+public class MainBanking {
+    public static void main(String[] args) {
+        System.out.println("The application is running");
+    }
+}
---
+++ UdemyTutorial/udemyTutorialChapter18/MainChallenge.java
@@ -0,0 +1,8 @@
+package udemyTutorialChapter18;
+
+public class MainChallenge {
+    public static void main(String[] args) {
+        Utilities util = new Utilities();
+        util.removePairs("AABCDDEFF");
+    }
+}
